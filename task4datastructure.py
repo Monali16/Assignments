5a606624851902a372653b54f0d69ba9a88bf053
Monali M Patel <patel205@gannon.edu>
2020-08-03 23:36:06 -0400

    task4.py

---
+++ task4datastructure.py
@@ -0,0 +1,18 @@
+e_list = [2,4,6]
+odd_list=[1,3,5]
+
+
+for i in range(1,50):
+    if(i%2==0):
+        e_list.append(i)
+    else:
+        odd_list.append(i)
+print("even list:", e_list)
+print("odd list:", odd_list)
+k = int(input("Please enter the number between 1 to 50: "))
+        
+
+ 
+
+ 
+
